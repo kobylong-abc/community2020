7702d26d24b5f05b9c79b660815a5b631faac914
kobylong-abc <kobylong@qq.com>
2020-12-15 17:23:47 +0800

    add bootstrap

--- src/main/java/com/kobylong/community/controller/HelloController.java
+++ src/main/java/com/kobylong/community/controller/HelloController.java
@@ -2,16 +2,15 @@ package com.kobylong.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(name= "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "greeting";
-
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 }
